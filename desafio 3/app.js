78054fd52a65265e558fa4e28b7c691149a87aa0
libonatii <analuzlibonati@gmail.com>
2024-07-19 02:44:58 -0300

    Contador até 10

---
+++ desafio 3/app.js
@@ -0,0 +1,13 @@
+// Criando contador que vai até 10 !
+
+let numeros = 1;
+let limite = 10;
+
+alert('Seja bem-vindo ao meu contador. Estarei contando de 1 a 10!');
+
+while(numeros <= limite) {
+    alert('Contagem: ${numeros}.');
+    numeros++;
+}
+
+    
