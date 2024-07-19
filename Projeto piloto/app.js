222e6c10fe94d42f45a7e3f6f0a63101343d0559
libonatii <analuzlibonati@gmail.com>
2024-07-19 03:28:47 -0300

    Jogo do Número Secreto finalizado

--- Projeto piloto/app.js
+++ Projeto piloto/app.js
@@ -1,26 +1,24 @@
-alert( 'Olá, mundo. Seja bem-vindo ao meu joguinho..');
-let numeroSecreto = 52;
-let chute 
+alert('Olá, mundo. Seja bem-vindo ao meu joguinho.');
+let numeroSecreto = parseInt(Math.random() * 55 + 1);
+let chute;
+let tentativas = 0;
 
-
-// condição de erro e acerto
-if (chute == numeroSecreto) {
-console.log('Ta fazendo o que aqui, Gabriel? Achou que ia ganhar britney? Mas parabens.');
-    alert('Parabéns, você acertou o número secreto ${numeroSecreto}!')
-} else {
-    alert('Tu errou meu nobre kkkkk')
-}
-
-
-//repetição para continuar o jogo
-while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 55.');
+// repetição para continuar o jogo
+while (chute != numeroSecreto) {
+    chute = parseInt(prompt('Escolha um número entre 1 e 55.'));
+    tentativas++;
     
-// condição pra diquinhas
-    if(chute > numeroSecreto) {
-        alert('Hmmm, tu errou! O número secreto é um pouco menor que esse aí! ${chute}')    
-}   else {
-        alert('Tenta de novo, o número secreto é maior que isso. ${chute}')
+    // condição pra diquinhas
+    if (chute > numeroSecreto) {
+        alert(`Hmmm, tu errou! O número secreto é um pouco menor que esse aí! Chute de número ${chute}. ${tentativas} tentativas e contando.`);
+    } else if (chute < numeroSecreto) {
+        alert(`Tenta de novo, o número secreto é maior que isso. Chute de número ${chute}. ${tentativas} tentativas e contando.`);
+    }
 }
 
-}   
+// Mensagem de parabéns
+if (tentativas > 1) {
+    alert(`Parabéns, você acertou o número secreto ${numeroSecreto}! Você ganhou o jogo com ${tentativas} tentativas!`);
+} else {
+    alert(`Parabéns, você acertou o número secreto ${numeroSecreto}! Você ganhou o jogo com ${tentativas} tentativa!`);
+} 
