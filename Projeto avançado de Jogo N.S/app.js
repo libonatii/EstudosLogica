fe65ca5eec4c34906538d2a361cf84dcd9dfa515
libonatii <analuzlibonati@gmail.com>
2024-07-22 22:08:04 -0300

    Função com parâmetros

--- Projeto avançado de Jogo N.S/app.js
+++ Projeto avançado de Jogo N.S/app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = 'Número Secreto';
+//let titulo = document.querySelector("h1");
+//titulo.innerHTML = 'Número Secreto';
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = 'Escolha um número de 1 a 55!';
+//let paragrafo = document.querySelector("p");
+//paragrafo.innerHTML = 'Escolha um número de 1 a 55!';
+
+function textoNaTela (tag, texto) {
+    let elemento = document.querySelector(tag);
+    elemento.innerHTML = texto 
+}
+
+textoNaTela("h1", 'Número Secreto');
+textoNaTela("p", 'Escolha um número de 1 a 55!');
 
 function verificarChute () {
-    console.log('O botão chute foi clicado!')
+    console.log('O botão chute foi clicado!');
 }
