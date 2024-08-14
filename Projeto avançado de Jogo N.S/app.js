2f7e142c0d36f9caeacf7dc1bab860ba898b1b81
libonatii <analuzlibonati@gmail.com>
2024-08-14 20:40:53 -0300

    Verificação de chute realizada

--- Projeto avançado de Jogo N.S/app.js
+++ Projeto avançado de Jogo N.S/app.js
@@ -1,21 +1,34 @@
-//let titulo = document.querySelector("h1");
-//titulo.innerHTML = 'Número Secreto';
-
-//let paragrafo = document.querySelector("p");
-//paragrafo.innerHTML = 'Escolha um número de 1 a 55!';
-
-function textoNaTela (tag, texto) {
+// função com parâmetros para aparecer texto na tela
+function textoNaTela(tag, texto) {
     let elemento = document.querySelector(tag);
-    elemento.innerHTML = texto 
+    elemento.innerHTML = texto;
 }
 
 textoNaTela("h1", 'Número Secreto');
 textoNaTela("p", 'Escolha um número de 1 a 55!');
 
-function verificarChute () {
+let numeroSecreto = gerarNumeroRandom(); // Gera o número secreto uma vez
+
+// função que verifica chute 
+function verificarChute() {
     console.log('O botão chute foi clicado!');
+    let chute = document.querySelector('input').value;
+
+    if (chute > numeroSecreto) {
+        alert('Hmm, acho que você chutou um pouco alto demais.');
+    } else if (chute < numeroSecreto) {
+        alert('Chuta um pouco mais alto.');
+    } else if (chute == numeroSecreto) {
+        textoNaTela('h1', 'Você acertou!');
+        textoNaTela('p', 'Parabéns, você achou o número secreto. :)');
+        alert('EITA NOISSS!!!');
+    }
 }
 
-function gerarNumeroRandom () {
-    return parseInt(Math.random() * 55 + 1)
+// função que gera número aleatório
+function gerarNumeroRandom() {
+    return parseInt(Math.random() * 55 + 1);
 }
+
+// Aqui você deve ligar a função `verificarChute` ao evento de clique do botão
+document.querySelector('button').addEventListener('click', verificarChute);
