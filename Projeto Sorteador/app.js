1af40ac07ba7c2af6b6153c96f2d547af35490c0
libonatii <analuzlibonati@gmail.com>
2024-09-04 23:39:48 -0300

    Update

--- Projeto Sorteador/app.js
+++ Projeto Sorteador/app.js
@@ -2,32 +2,25 @@ function sortear() {
     let qntd = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    alert(numero);
     let sorteados = [];
     let numero;
 
-
     //array para armazenar os números sorteados
-    for (let i = 0; i < quantidade; i++) {
+    for (let i = 0; i < qntd; i++) {  
         numero = obterNumero(de, ate);
         sorteados.push(numero);
-        alert(sorteados);
+        alert(numero); 
     }
 
-gerarSorteio(qntd,de,ate);
-
+    gerarSorteio(qntd, de, ate);
 }
 
-function textoNaTela(tag,texto) {
+function textoNaTela(tag, texto) {
     let elemento = document.querySelector(tag);
     elemento.innerHTML = texto;
-
 }
-
 
 function obterNumero(min, max) {
     console.log('O botão sortear foi clicado.');
-    let valorQntd = parseInt(document.querySelector('.container__botao').value);
-    return Math.floor(Math.random() * (max - min + 1)) +  min;
-
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
